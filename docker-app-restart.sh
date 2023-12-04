8ea559140bd33d7ccf8c28e980904e19cb846516
skyglass <skyglass2001@gmail.com>
2023-12-04 01:16:59 +0100

    started implementing  minio image gateway

--- docker-app-restart.sh
+++ docker-app-restart.sh
@@ -2,6 +2,10 @@ docker-compose -f docker-app-compose.yml down
 
 mvn clean install -DskipTests
 
+cd ../basket-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=basket-service
+
 cd ./catalog/catalog-command-service
 mvn spring-boot:build-image -DskipTests \
   -Dspring-boot.build-image.imageName=catalog-command-service
@@ -9,6 +13,22 @@ mvn spring-boot:build-image -DskipTests \
 cd ../../catalog/catalog-query-service
 mvn spring-boot:build-image -DskipTests \
   -Dspring-boot.build-image.imageName=catalog-query-service
+
+cd ../../infrastructure/flux-gateway-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=flux-gateway-service
+
+cd ../../infrastructure/graphql-gateway-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=graphql-gateway-service
+
+cd ../../infrastructure/image-gateway-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=image-gateway-service
+
+cd ../order-processing-service
+mvn spring-boot:build-image -DskipTests \
+  -Dspring-boot.build-image.imageName=order-processing-service
 
 cd ../../gateway-service
 mvn spring-boot:build-image -DskipTests \
